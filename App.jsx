a39034daf2218763f0bb0d098d9c3d7a11abb75e
AlexSvir <alex.svir.dev@gmail.com>
2025-04-19 10:09:30 -0400

    Feat: Resume output layout

---
+++ App.jsx
@@ -0,0 +1,448 @@
+import React, { useState, useEffect } from 'react';
+import {
+	View,
+	SectionList,
+	Pressable,
+	Text,
+	TextInput,
+	Button,
+	Switch,
+	Alert,
+	StyleSheet
+} from 'react-native';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+import RNFS from 'react-native-fs';
+
+import { persconf } from './persconfig';
+
+
+
+export default function App() {
+	const [company, setCompany] = useState('');
+	const [position, setPosition] = useState('');
+	const [skillList, setSkillList] = useState([]);
+	const [switches, setSwitches] = useState({});
+
+
+	async function generatePdf() {
+		try {
+			const coverLetter = await RNHTMLtoPDF.convert({
+				html: generateCoverLetter({
+					position,
+					company,
+					date: new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }),
+					skills: Object.values(switches).flatMap( sect => Object.entries(sect).filter(([key, [cl,res]]) => cl).map(([key, val]) => key) )
+					//skills: Object.values(switches).flatMap( sect => Object.entries(sect).filter(e => e[1][1]).map(e => e[0]) )
+				}),
+				fileName: 'CoverLetter',
+				directory: 'Applicator'
+			});
+			const resume = await RNHTMLtoPDF.convert({
+				html: generateResume({
+					...DEFAULT_RESUME_PARAMS,
+					position,
+					skills: Object.values(switches).flatMap( sect => Object.entries(sect).filter(([key, [cl,res]]) => res).map(([key, val]) => key) )
+					//skills: Object.values(switches).flatMap( sect => Object.entries(sect).filter(e => e[1][0]).map(e => e[0]) )
+				}),
+				fileName: 'Resume',
+				directory: 'Applicator'
+			});
+			Alert.alert('Generated PDF', `Saved at\n\n${coverLetter.filePath}\n\n${resume.filePath}`);
+		} catch(err) {
+			Alert.alert('ERROR', err.message);
+		}
+	}
+
+	useEffect(
+		() => {
+			async function foo() {
+				const path = RNFS.ExternalStorageDirectoryPath + '/Documents/Applicator/presets/skills.txt';
+				try {
+					const content = await RNFS.readFile(path);
+					const arr = content.toString().split('\n\n').filter(d => d).map( d => d.split('\n').filter( i => i ) );
+					const sw = Object.fromEntries(
+						arr.map( a => [ a[0], Object.fromEntries(
+							a.filter((x,i) => i > 0).map( b => [b, [false, false]] )
+						) ] )
+					);
+					setSkillList(arr);
+					setSwitches(sw);
+				} catch(err) {
+					try {
+						const content = DEFAULT_SKILLS_FILE_CONTENT;
+						await RNFS.writeFile(path, content, 'utf8');
+						const arr = content.toString().split('\n\n').filter(d => d).map( d => d.split('\n').filter( i => i ) );
+						const sw = Object.fromEntries(
+							arr.map( a => [ a[0], Object.fromEntries(
+								a.filter((x,i) => i > 0).map( b => [b, [false, false]] )
+							) ] )
+						);
+						setSkillList(arr);
+						setSwitches(sw);
+					} catch (err) {
+						Alert.alert('Skills loading failed', err.message);
+					}
+				}
+			};
+			foo();
+		},
+		[]
+	);
+
+	return	<View>
+				<View
+					style={styles.genButton}>
+					<Button
+						title="PDF"
+						onPress={ () => generatePdf() }
+						disabled={!company || !position}
+					/>
+				</View>
+				<Text style={styles.label}>Company</Text>
+				<TextInput
+					style={styles.input}
+					value={company}
+					placeholder={'Company Name'}
+					onChangeText={ txt => setCompany(txt) } />
+				<Text style={styles.label}>Position</Text>
+				<TextInput
+					style={styles.input}
+					value={position}
+					placeholder={'Position'}
+					onChangeText={ txt => setPosition(txt) } />
+				<Skills
+					skills={skillList}
+					switches={switches}
+					onSwitch={ (section, skill, idx, val) => setSwitches({
+						...switches,
+						[section]: {
+							...switches[section],
+							[skill]: [
+								idx ? switches[section][skill][0] : val,
+								idx ? val : switches[section][skill][1]
+							]
+						}
+					}) }
+				/>
+			</View>;
+}
+
+
+function Skills({ skills, switches, onSwitch }) {
+	const [selected, setSelected] = useState('');
+
+	const content = skills.map( sk => Object.fromEntries([
+		['nm', sk[0]],
+		['data', sk.slice(1)]
+	]) );
+
+	return (
+		<SectionList
+			sections={content}
+			renderItem={ props => props.section.nm !== selected ? null :
+				<SkillItem
+					{...props}
+					switches={switches[props.section.nm][props.item]}
+					onSwitch={ (idx, val) => onSwitch(props.section.nm, props.item, idx, val) }
+				/> }
+			renderSectionHeader={ ({section: {nm}}) =>
+				<SkillHeader
+					key={nm}
+					text={nm}
+					selected={nm === selected}
+					switches={switches[nm]}
+					onPress={ () => setSelected(selected === nm ? '' : nm) }
+				/> }
+		/>
+	);
+}
+
+function SkillHeader({ text, selected, switches, onPress }) {
+	const [clCnt, resCnt, totalCnt] = Object.values(switches).reduce(
+		(acc, [cl,res]) => {
+			if (cl) acc[0]++;
+			if (res) acc[1]++;
+			acc[2]++;
+			return acc;
+		},
+		[0, 0, 0]
+	);
+
+	return (
+		<Pressable onPress={onPress}>
+			<Text style={styles.skillHeader}>
+				{selected ? '\u261F' : '\u261E'}
+				&emsp;
+				{text}
+				&emsp;
+				{`${totalCnt}>> ${clCnt} / ${resCnt}`}
+			</Text>
+		</Pressable>
+	);
+}
+
+function SkillItem({ item, index, section, separators, switches, onSwitch }) {
+
+	return (
+		<View style={styles.skillItem}>
+			<Text style={styles.skillName}>{index + 1}.) {item ?? '--'}</Text>
+				<Switch value={switches[0]} onValueChange={ val => onSwitch(0, val) } />
+				<Switch value={switches[1]} onValueChange={ val => onSwitch(1, val) } />
+		</View>
+	);
+}
+
+//**************************************************************************************************\\
+//**************************************************************************************************\\
+
+const styles = StyleSheet.create({
+	genButton: {
+		marginHorizontal: 8,
+		marginVertical: 25
+	},
+	label: {
+		marginHorizontal: 8,
+		marginTop: 4
+	},
+	input: {
+		marginHorizontal: 8,
+		marginBottom: 4,
+		borderColor: 'blue',
+		borderWidth: 2,
+		borderRadius: 4
+	},
+	skillHeader: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		backgroundColor: 'aquamarine',
+		borderColor: 'white',
+		borderWidth: 1,
+		padding: 5
+	},
+	skillItem: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		borderColor: 'red',
+		borderWidth: 1,
+		backgroundColor: '#cf4',
+		paddingHorizontal: 6,
+		paddingVertical: 2
+	},
+	skillName: {
+		flexGrow: 1,
+		backgroundColor: 'yellow',
+		marginLeft: 6
+	}
+});
+
+/**********************************************************************************************************\
+ *
+ *
+\**********************************************************************************************************/
+
+const DEFAULT_SKILLS_FILE_CONTENT =
+`Titles
+C++
+Android
+Java
+JavaScript
+Assembly
+Microcontrollers
+ARM
+Arduino
+HTML
+XML
+Linux
+SQL
+
+Android
+Multi-threading
+SQL Databases
+Bluetooth
+Input Method Editor
+Foreground Service
+Sockets
+Screen Overlay
+
+C++
+Multi-threading
+Generics
+Standard library
+
+Java
+Multi-threading
+Generics
+SQL databases
+
+Soft
+Analytical thinking
+Strong work ethics
+Quick learning
+Detail oriented
+Critical thinking
+Goal-oriented
+Experimenting
+Listening`;
+
+const DEFAULT_COVER_LETTER_PARAMS = {
+	position: 'Entry-Level Software Developer',
+	company: 'Company',
+	date: 'MM/DD/YYYY',
+	skills: [
+		'Multi-threading', 'SQL Databases', 'Bluetooth',
+		'Input Method Editor', 'Foreground Service', 'Sockets',
+		'Screen Overlay'
+	]
+};
+
+const DEFAULT_RESUME_PARAMS = {
+	position: 'Entry-Level Software Developer',
+	skills: [
+		'C++', 'Android', 'Java', 'Analytical thinking',
+		'Strong work ethics', 'Quick learning', 'Detail oriented',
+		'Critical thinking', 'Goal-oriented', 'Listening'
+	],
+	certificates: [
+		'Programming Hub C++ Certification Course',
+		'Programming Hub C++ Advanced Certification Course',
+		'Programming Hub Java Certification Course',
+		'Programming Hub Java Advanced Certification Course',
+		'Programming Hub Android Development Certification Course'
+	]
+};
+
+
+function generateCoverLetter({ position, company, date, skills }) {
+	return `
+<html>
+<head>
+	<style>
+		body {
+			width: 8.5in;
+			height: 11in;
+			padding: 0.79in;
+		}
+		h1,p.hdr {
+			margin: 0;
+		}
+		p.pos {
+			font-size: 1.5em;
+			margin-bottom: 0.5em;
+		}
+	</style>
+</head>
+<body>
+
+	<h1>${persconf.name}</h1>
+	<p class="hdr pos">${position}</p>
+	<br>
+	<p>Phone ${persconf.phone}</p>
+	<p>Email <a href="mailto:${persconf.email}">${persconf.email}</a></p>
+	<p>GitHub <a href="https://github.com/${persconf.github}">https://github.com/${persconf.github}</a></p>
+	<br>
+	<p>Brighton, ${date}</p>
+	<br>
+	<br>
+	<p>Dear ${company} Team,</p>
+
+	<p>I was exited to find your posting for the position of ${position}.
+	My way is out of the ordinary, almost 20 years I have studied programming
+	by solving a wide  variety of practical problems. Each of my projects was
+	a challenge, I went step by step from easy things to more complicated ones,
+	so I was forced to resolve lots of minor problems on my way to my major goal.</p>
+
+	<p>Over time, my enthusiasm and thirst for learning did not fade, but only
+	intensified as I dove into computer science. Started with a simple curiosity,
+	my programming hobby has turned into a real passion and now my mind is
+	totally focused on it 24/7.</p>
+
+	<p>When I cannot access my laptop, I read educational and tutorial
+	publications or books, search the ways of resolving my current
+	projects’ problems, analyze and plan my apps development. I never
+	remember my night-dreams but it seems they are all about coding too.</p>
+
+	<p>During many years of programming I developed a lot of various applications,
+	mostly covering my current needs. So I experienced many experiments and learned
+	many APIs, interfaces and technologies e. g.:</p>
+
+	<ul>${skills.map(li => '<li>' + li + '</li>').join('')}
+	</ul>
+
+	<p>But at the same time, I have never thought this can become my occupation
+	or source of money. So, now it’s time to direct my passion and desire for
+	the benefit of humanity and ${company}.</p>
+
+	<p>Can’t wait for instructions regarding my next steps from you! I’m eager
+	to direct my skills and passion to meet business needs.</p>
+
+	<p>Sincerely,</p>
+	<p>${persconf.name}</p>
+</body>
+</html>
+	`;
+}
+
+function generateResume({ position, skills, certificates }) {
+	return `
+<html>
+<head>
+	<style>
+		body {
+			width: 8.5in;
+			height: 11in;
+			padding: 0.79in;
+		}
+		h1,p.hdr {
+			margin: 0;
+		}
+		p.pos {
+			font-size: 1.5em;
+			margin-bottom: 0.5em;
+		}
+	</style>
+</head>
+<body>
+
+	<h1>${persconf.name}</h1>
+	<p class="hdr pos">${position}</p>
+	<p class="hdr">${persconf.phone}</p>
+	<p class="hdr"><a href="mailto:${persconf.email}">${persconf.email}</a></p>
+	<p class="hdr"><a href="https://github.com/${persconf.github}">https://github.com/${persconf.github}</a></p>
+	<br>
+
+	<p>Enthusiastic, motivated, self-educated ${position} with strong practical
+	background and great passion to coding. Spent almost 20 years learning C++,
+	Java, Android and never stop developing, even when sleeping. Eager to direct
+	my skills and passion to meet business needs.</p>
+
+	<h2>Education</h2>
+
+	<ul>Self education based on:
+		<li>Almost 20 years of learning, developing, experimenting, investigating computer technologies</li>
+    	<li>1000s hours of coding, debugging, fixing errors, resolving compiling problems</li>
+    	<li>Great practical experience in C++, Android, Java</li>
+    </ul>
+
+	<ul>Belarusian State Medical University
+		<li>2005 – 2011</li>
+	</ul>
+
+	<h2>Skills</h2>
+	<ul>${skills.map( li => '<li>' + li + '</li>' ).join('')}
+	</ul>
+
+	<h2>Certification</h2>
+	<ul>${certificates.map( li => '<li>' + li + '</li>' ).join('')}
+	</ul>
+
+	<h2>Languages</h2>
+	<ul>
+		<li>English</li>
+		<li>Belarusian</li>
+		<li>Russian</li>
+    </ul>
+</body>
+</html>
+	`;
+}
